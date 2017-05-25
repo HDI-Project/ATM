bdeb2137886550542c85cb6d4883ece24bb4f44d
Thomas Swearingen <swearin3@msu.edu>
2017-05-24 22:40:39 -0400

    Update query script
    Update learner query script so that it also prints the frozens64
    parameters.

--- query_learner_params.py
+++ query_learner_params.py
@@ -1,9 +1,12 @@
 from delphi.database import *
+from delphi.utilities import Base64ToObject
 
 
 learner_id_number = 31
 
 learner = GetLearner(learner_id_number)
+frozen = GetFrozenSet(learner.frozen_set_id,False)
+
 
 print 'The param64 values are:'
 for key, value in learner.params.iteritems():
@@ -12,6 +15,10 @@ for key, value in learner.params.iteritems():
 print 'The trainable_param64 values are:'
 for key, value in learner.trainable_params.iteritems():
     print '\t{} = {}'.format(key, value)
+
+print 'The frozens64 values are:'
+for idx in range(len(frozen.frozens)):
+    print '\t{} = {}'.format(frozen.frozens[idx][0], frozen.frozens[idx][1])
 
 if learner.errored is None:
     print 'Test Accuracy was {}'.format(learner.test)
