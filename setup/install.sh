db2b6d28049d273d1d7ecaa650bf7cc682cb1610
Bennett Cyphers <bcyphers@mit.edu>
2017-10-27 13:48:02 -0400

    add install script

---
+++ setup/install.sh
@@ -0,0 +1,13 @@
+# change to the root directory
+cd $(git rev-parse --show-toplevel)
+
+# install sqlite
+sudo apt install -y sqlite3
+
+# set up virtual environment; install python dependencies
+virtualenv venv
+. venv/bin/activate
+pip install -r requirements.txt
+
+# set up sqlite database
+sqlite3 btb.db < setup/sqlite-setup.sql
