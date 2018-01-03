32f6d90b33cb4432cc6632824492e7a9cf002ba2
Bennett Cyphers <bcyphers@mit.edu>
2018-01-03 14:28:07 -0500

    update BTB test

--- test/btb_test.py
+++ test/btb_test.py
@@ -13,24 +13,11 @@ from utilities import *
 
 
 CONF_DIR = 'config/test/btb/'
-BASELINE_PATH = 'test/baselines/best_so_far/'
 RUN_CONFIG = join(CONF_DIR, 'run.yaml')
 SQL_CONFIG = join(CONF_DIR, 'sql.yaml')
 
 DATA_URL = 'https://s3.amazonaws.com/mit-dai-delphi-datastore/downloaded/'
 
-DATASETS_MAX_MIN = [
-    'wholesale-customers_1.csv',
-    'car_1.csv',
-    'wall-robot-navigation_1.csv',
-    'wall-robot-navigation_2.csv',
-    'wall-robot-navigation_3.csv',
-    'analcatdata_authorship_1.csv',
-    'cardiotocography_1.csv',
-    'wine_1.csv',
-    'seismic-bumps_1.csv',
-    'balance-scale_1.csv',
-]
 DATASETS_MAX_FIRST = [
     'wine_1.csv',
     'balance-scale_1.csv',
@@ -48,35 +35,41 @@ parser = argparse.ArgumentParser(description='''
 Test the performance of a new selector or tuner and compare it to that of other
 methods.
 ''')
-add_arguments_sql(parser)
-add_arguments_datarun(parser)
 parser.add_argument('--processes', help='number of processes to run concurrently',
-                    type=int, default=4)
+                    type=int, default=1)
+parser.add_argument('--tuner', type=option_or_path(TUNERS),
+                    help='type of, or path to, BTB tuner')
+parser.add_argument('--selector', type=option_or_path(SELECTORS),
+                    help='type of, or path to, BTB selector')
+parser.add_argument('--graph', action='store_true', default=False,
+                    help='if this flag is inculded, graph the best-so-far '
+                    'results of each datarun against the baseline.')
+parser.add_argument('--dataruns', nargs='+', type=int,
+                    help='(optional) IDs of previously-created dataruns to graph. '
+                    'If this option is included, no new dataruns will be created.')
 args = parser.parse_args()
 
 sql_conf, run_conf, _ = load_config(sql_path=SQL_CONFIG,
                                     run_path=RUN_CONFIG,
                                     args=args)
 db = Database(**vars(sql_conf))
-datarun_ids = {}
+datarun_ids = args.dataruns or []
+baselines = os.listdir(BASELINE_PATH)
+datasets = [d for d in DATASETS_MAX_FIRST if d in baselines]
 
-datasets = os.listdir(BASELINE_PATH)
-datasets = datasets[:5]
-print('using datasets', ', '.join(datasets))
-
-# generate datasets and dataruns
-for ds in datasets:
-    run_conf.train_path = DATA_URL + ds
-    run_conf.dataset_id = None
-    datarun_ids[ds] = enter_datarun(sql_conf, run_conf)
+# if necessary, generate datasets and dataruns
+if not datarun_ids:
+    for ds in datasets:
+        run_conf.train_path = DATA_URL + ds
+        run_conf.dataset_id = None
+        print('Creating datarun for', run_conf.train_path)
+        datarun_ids.append(enter_datarun(sql_conf, run_conf))
 
 # work on the dataruns til they're done
-work_parallel(db=db, datarun_ids=datarun_ids.values(),
-              n_procs=args.processes)
+print('Working on %d dataruns' % len(datarun_ids))
+work_parallel(db=db, datarun_ids=datarun_ids, n_procs=args.processes)
+print('Finished!')
 
 # graph the results
-for ds in datasets:
-    with open(join(BASELINE_PATH, ds)) as f:
-        baseline = [float(l.strip()) for l in f]
-    test = get_best_so_far(db, datarun_ids[ds])
-    graph_series(100, ds, baseline=baseline, test=test)
+for rid in datarun_ids:
+    report_auc_vs_baseline(db, rid, graph=args.graph)
