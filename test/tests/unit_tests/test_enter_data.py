49e0c9fc659cf4f2432a3214f1da5f9b2e9654ac
Bennett Cyphers <bcyphers@mit.edu>
2018-01-18 13:13:16 -0500

    updates to enter_data tests

--- test/tests/unit_tests/test_enter_data.py
+++ test/tests/unit_tests/test_enter_data.py
@@ -9,6 +9,25 @@ from atm.enter_data import enter_data, create_dataset, create_datarun
 
 
 DB_PATH = os.path.join(PROJECT_ROOT, 'test/atm.db')
+DATA_URL = 'https://s3.amazonaws.com/mit-dai-delphi-datastore/downloaded/'
+BASELINE_PATH = os.path.join(PROJECT_ROOT, 'test/baselines/best_so_far/')
+BASELINE_URL = 'https://s3.amazonaws.com/mit-dai-delphi-datastore/best_so_far/'
+
+METHOD_HYPERPARTS = {
+	'logreg': 6,
+	'svm': 4,
+	'sgd': 24,
+	'dt': 2,
+	'et': 2,
+	'rf': 2,
+	'gnb': 1,
+	'mnb': 1,
+	'bnb': 1,
+	'gp': 8,
+	'pa': 4,
+	'knn': 24,
+	'mlp': 60,
+}
 
 
 @pytest.fixture()
@@ -26,24 +45,23 @@ def dataset(db):
         return create_dataset(db, 'class', data_path)
 
 
-def test_enter_dataset():
-    run_conf = RunConfig(train_path='https://')
-    sql_conf = SQLConfig(database=DB_PATH)
-    db = Database(**vars(sql_conf))
+def test_enter_dataset(db):
+	train_url = DATA_URL + 'pollution_1_train.csv'
+	test_url = DATA_URL + 'pollution_1_test.csv'
+    run_conf = RunConfig(train_path=train_url)
 
     enter_dataset(db, run_config)
-    assert os.path.exists(local_path)
+    assert os.path.exists(train_path_local)
 
 
-def test_enter_data_methods(dataset):
-    run_conf = RunConfig(dataset_id=dataset.id)
+def test_enter_datarun_by_methods(dataset):
     sql_conf = SQLConfig(database=DB_PATH)
     db = Database(**vars(sql_conf))
+    run_conf = RunConfig(dataset_id=dataset.id)
 
-    method_hyperparts = json.load()
-    for method, n_parts in method_hyperparts:
+    for method, n_parts in METHOD_HYPERPARTS:
         run_conf.methods = [method]
-        run_id = enter_data(sql_conf, run_conf)
+        run_id = enter_datarun(sql_conf, run_conf)
 
         assert db.get_datarun(run_id)
         with db_session(db):
@@ -52,23 +70,26 @@ def test_enter_data_methods(dataset):
             assert len(run.hyperpartitions) == n_parts
 
 
-def test_enter_data_all():
-    run_conf = RunConfig(dataset_id=dataset.id)
+def test_enter_datarun_all(dataset):
     sql_conf = SQLConfig(database=DB_PATH)
     db = Database(**vars(sql_conf))
+    run_conf = RunConfig(dataset_id=dataset.id)
 
-    method_hyperparts = json.load()
-    for method, n_parts in method_hyperparts:
-        run_conf.methods = [method]
-        run_id = enter_data(sql_conf, run_conf)
+    run_id = enter_datarun(sql_conf, run_conf)
 
-        assert db.get_datarun(run_id)
-        with db_session(db):
-            run = db.get_datarun(run_id)
-            assert run.dataset.id == dataset.id
-            assert len(run.hyperpartitions) == n_parts
+    with db_session(db):
+        run = db.get_datarun(run_id)
+        assert run.dataset.id == dataset.id
+        assert len(run.hyperpartitions) == sum(METHOD_HYPERPARTS.values())
 
 
-def test_run_per_partition(db):
-    run_conf = RunConfig(methods=['logreg'])
-    sql_conf = SQLConfig()
+def test_run_per_partition(dataset):
+    sql_conf = SQLConfig(database=DB_PATH)
+    run_conf = RunConfig(dataset_id=dataset.id, methods=['logreg'])
+
+    run_id = enter_datarun(sql_conf, run_conf, run_per_partition)
+
+    with db_session(db):
+        runs = db.get_datarun(run_id)
+        assert len(runs) == METHOD_HYPERPARTS['logreg']
+		assert all([len(run.hyperparpartitions) == 1 for run in runs])
