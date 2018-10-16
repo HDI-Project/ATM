300870c2f37ba69a80a64446171be1727afc0c5f
Albert Carter <albert.r.carter@gmail.com>
2018-10-16 14:44:11 +0200

    add operations for get datasets. Set doesn't show yet

--- scripts/rest_api_server.py
+++ scripts/rest_api_server.py
@@ -90,15 +90,15 @@ def encode_entity(entity=[]):
 
 def get_operator_fn(op):
     return {
-        '=' : operator.eq,
-        '>' : operator.gt,
-        'gt' : operator.gt,
-        '>=' : operator.ge,
-        'ge' : operator.ge,
-        '<' : operator.lt,
-        'lt' : operator.lt,
-        '<=' : operator.le,
-        'le' : operator.le,
+        '=': operator.eq,
+        '>': operator.gt,
+        'gt': operator.gt,
+        '>=': operator.ge,
+        'ge': operator.ge,
+        '<': operator.lt,
+        'lt': operator.lt,
+        '<=': operator.le,
+        'le': operator.le,
         }.get(op, operator.eq)
 
 
@@ -106,16 +106,16 @@ db = set_up_db()
 app, api, ns = set_up_flask()
 
 
-def set_up_dataset_parser():
-    dataset_parser = api.parser()
-
+def return_get_dataset_parser():
     comparison_args = [
         ('id', int), ('name', str), ('train_path', str), ('test_path', str),
         ('description', str), ('n_examples', int), ('k_classes', int),
-        ('majority', float), ('size_kb', int)]
+        ('d_features', int), ('majority', float), ('size_kb', int)]
     operation_args = [
         ('n_examples_op', str), ('k_classes_op', str), ('d_features_op', str),
         ('majority_op', str), ('size_kb_op', str)]
+
+    dataset_parser = api.parser()
     for col_tuple in comparison_args:
         dataset_parser.add_argument(col_tuple[0], type=col_tuple[1])
     for col_tuple in operation_args:
@@ -125,21 +125,65 @@ def set_up_dataset_parser():
     return dataset_parser
 
 
-dataset_parser = set_up_dataset_parser()
+def return_set_dataset_parser():
+    args = [
+        ('name', str), ('description', str), ('train_path', str),
+        ('test_path', str), ('class_column', str), ('n_examples', int),
+        ('k_classes', int), ('d_features', str), ('majority', str)]
 
+    dataset_parser = api.parser()
+    for col in args:
+        dataset_parser.add_argument(col[0], type=col[1])
+
+    return dataset_parser
+
+
+get_dataset_parser = return_get_dataset_parser()
+set_dataset_parser = return_set_dataset_parser()
 
 @ns.route('/datasets')
-@api.expect(dataset_parser)
+
+
 class Dataset(Resource):
     @ns.doc('get some or all datasets')
+    @api.expect(get_dataset_parser)
     def get(self):
-        args = dataset_parser.parse_args()
+        args = get_dataset_parser.parse_args()
         args['entity_id'] = args.get('id', None)
-        args['n_examples_op'] = get_operator_fn(args.get('n_examples_op', None))
         args.pop('id', None)
+
+        # deal with operations
+        args['n_examples_op'] = get_operator_fn(args.get('n_examples_op', None))  # noqa
+        args['k_classes_op'] = get_operator_fn(args.get('n_examples_op', None))
+        args['d_features_op'] = get_operator_fn(args.get('n_examples_op', None))  # noqa
+        args['majority_op'] = get_operator_fn(args.get('n_examples_op', None))
+        args['size_kb_op'] = get_operator_fn(args.get('n_examples_op', None))
 
         res = encode_entity(db.get_datasets(**args))
         return json.loads(res)
+
+    @ns.doc('create a dataset')
+    # @api.expect(set_dataset_parser)
+    def set(self):
+        dataset_parser = return_set_dataset_parser()
+        args = dataset_parser.parse_args()
+
+        meta = MetaData(
+            args['class_column'], args['train_path'], args['test_path'])
+        args['size_kb'] = old_div(meta.size, 1000)
+
+        dataset = db.create_dataset(**args)
+
+        dataset = db.create_dataset(name=name,
+                                    description=run_config.data_description,
+                                    train_path=run_config.train_path,
+                                    test_path=run_config.test_path,
+                                    class_column=run_config.class_column,
+                                    n_examples=meta.n_examples,
+                                    k_classes=meta.k_classes,
+                                    d_features=meta.d_features,
+                                    majority=meta.majority,
+                                    size_kb=old_div(meta.size, 1000))
 
 
 if __name__ == '__main__':
