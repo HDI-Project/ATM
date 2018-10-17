0fefbc1395ca481dfc240e450d128acb62412a92
Albert Carter <albert.r.carter@gmail.com>
2018-10-17 18:21:49 +0200

    metaparser works

--- scripts/api/parsers.py
+++ scripts/api/parsers.py
@@ -1,3 +1,113 @@
+import operator
+import os
+
+
+from atm.config import load_config
+from atm.database import Database
+
+
+def set_up_db():
+    sql_config_path = os.path.join('config', 'sql.yaml')
+    sql_conf = load_config(sql_path=sql_config_path)[0]
+
+    # YOU NEED TO redo SQL_CONF path, or get database to accept somethign in a
+    # higher directory
+
+    sql_conf.database = os.path.join(sql_conf.database)
+
+    db = Database(sql_conf.dialect, sql_conf.database, sql_conf.username,
+                  sql_conf.password, sql_conf.host, sql_conf.port,
+                  sql_conf.query)
+
+    return db
+
+
+class Metaparser:
+    def __init__(self, target_entity, db, column_args, op_args=[]):
+        self.target_entity = target_entity
+        self.column_args = column_args
+        self.op_args = op_args
+        self.db = db
+        self.parser = None
+
+    def set_flaskplus_parser(self, api):
+        """ returns a flaskplus api parser for use in the API"""
+        temp_parser = api.parser()
+
+        all_args = self.column_args + self.op_args
+
+        for arg in all_args:
+            temp_parser.add_argument(name=arg.name, type=arg.type, help=arg.help)
+
+        self.parser = temp_parser
+
+
+class Arg:
+    def __init__(self, target_col, name, input_type=str, required=False,
+                 help_str=''):
+        self.name = name
+        self.type = input_type
+        self.target_col = target_col
+        self.required = required
+        self.help = help_str
+
+
+class OpArg(Arg):
+    def __init__(self, target_col, name, input_type=str, required=False,
+                 help_str=''):
+        super().__init__(target_col, name, input_type, required, help_str)
+
+        self.help = 'comparison operator. i.e. =, >, >='
+
+    def convert_to_operation(self, string_rep):
+        op_map = {
+            '=': operator.eq,
+            '>': operator.gt,
+            'gt': operator.gt,
+            '>=': operator.ge,
+            '=>': operator.ge,
+            'ge': operator.ge,
+            '<': operator.lt,
+            'lt': operator.lt,
+            '<=': operator.le,
+            '=<': operator.le,
+            'le': operator.le}
+
+        return op_map.get(string_rep, operator.eq)
+
+
+db = set_up_db()
+ds = db.Dataset
+
+dataset_args = [
+    Arg(target_col=ds.id, name='entity_id', input_type=int, required=False),
+    Arg(ds.name, 'name', str, False),
+    Arg(ds.description, 'description', str, False),
+    Arg(ds.train_path, 'train_path', str, False),
+    Arg(ds.test_path, 'test_path', str, False),
+    Arg(ds.class_column, 'class_column', str, False),
+    Arg(ds.n_examples, 'n_examples', int, False),
+    Arg(ds.k_classes, 'k_classes', int, False),
+    Arg(ds.d_features, 'd_features', int, False),
+    Arg(ds.majority, 'majority', float, False)]
+
+operation_args = [
+    OpArg(ds.n_examples, 'n_examples_op', str, False),
+    OpArg(ds.k_classes, 'k_classes_op', str, False),
+    OpArg(ds.d_features, 'd_features_op', str, False),
+    OpArg(ds.majority, 'majority_op', str, False),
+    OpArg(ds.size_kb, 'size_kb_op', str, False)]
+
+metaparser_for_dataset_get = Metaparser(ds, db, dataset_args, operation_args,)
+metaparser_for_dataset_post = Metaparser(ds, db, dataset_args, [])
+
+
+
+
+
+
+
+
 def return_get_dataset_parser(api):
     operation_args = [
         ('n_examples_op', str), ('k_classes_op', str), ('d_features_op', str),
--- scripts/rest_api_server.py
+++ scripts/rest_api_server.py
@@ -5,86 +5,83 @@ from flask_restplus import Resource
 
 from atm.encoder import MetaData
 from api.encoders import encode_entity, get_operator_fn
-from api.parsers import return_get_dataset_parser, return_set_dataset_parser, return_put_dataset_parser
+from api.parsers import metaparser_for_dataset_get
 from api.setup import set_up_db, set_up_flask
 
 
-db = set_up_db()
+# db = set_up_db()
 app, api, ns = set_up_flask()
 
-
-get_dataset_parser = return_get_dataset_parser(api)
-set_dataset_parser = return_set_dataset_parser(api)
-put_dataset_parser = return_put_dataset_parser(api)
+metaparser_for_dataset_get.set_flaskplus_parser(api)
 
 
 @ns.route('/datasets')
 class Dataset(Resource):
     @ns.doc('get some or all datasets')
-    @api.expect(get_dataset_parser)
+    @api.expect(metaparser_for_dataset_get.parser)
     def get(self):
-        args = get_dataset_parser.parse_args()
+        args = metaparser_for_dataset_get.parser.parse_args()
         args['entity_id'] = args.get('id', None)
         args.pop('id', None)
 
-        # deal with operations
-        args['n_examples_op'] = get_operator_fn(args.get('n_examples_op', None))  # noqa
-        args['k_classes_op'] = get_operator_fn(args.get('n_examples_op', None))
-        args['d_features_op'] = get_operator_fn(args.get('n_examples_op', None))  # noqa
-        args['majority_op'] = get_operator_fn(args.get('n_examples_op', None))
-        args['size_kb_op'] = get_operator_fn(args.get('n_examples_op', None))
-        res = encode_entity(db.get_datasets(**args))
+        # change operation strings to actual operations
+        for op_arg in metaparser_for_dataset_get.op_args:
+            string_op = args[op_arg.name]
+            operation = op_arg.convert_to_operation(string_op)
+            args[op_arg.name] = operation
+
+        res = encode_entity(metaparser_for_dataset_get.db.get_datasets(**args))
         return json.loads(res)
 
-    @ns.doc('create a dataset')
-    @api.expect(set_dataset_parser)
-    def post(self):
-        args = set_dataset_parser.parse_args()
+    # @ns.doc('create a dataset')
+    # @api.expect(set_dataset_parser)
+    # def post(self):
+    #     args = set_dataset_parser.parse_args()
 
-        meta = MetaData(
-            args['class_column'], args['train_path'], args['test_path'])
+    #     meta = MetaData(
+    #         args['class_column'], args['train_path'], args['test_path'])
 
-        args['size_kb'] = old_div(meta.size, 1000)
+    #     args['size_kb'] = old_div(meta.size, 1000)
 
-        dataset = db.create_dataset(**args)
+    #     dataset = db.create_dataset(**args)
 
-        try:
-            dataset = db.create_dataset(
-                name=args.get('name'),
-                description=args.get('data_description'),
-                train_path=args.get('train_path'),
-                test_path=args.get('test_path'),
-                class_column=args.get('class_column'),
-                n_examples=args.get('n_examples'),
-                k_classes=args.get('k_classes'),
-                d_features=args.get('d_features'),
-                majority=args.get('majority'),
-                size_kb=old_div(meta.size, 1000))
+    #     try:
+    #         dataset = db.create_dataset(
+    #             name=args.get('name'),
+    #             description=args.get('data_description'),
+    #             train_path=args.get('train_path'),
+    #             test_path=args.get('test_path'),
+    #             class_column=args.get('class_column'),
+    #             n_examples=args.get('n_examples'),
+    #             k_classes=args.get('k_classes'),
+    #             d_features=args.get('d_features'),
+    #             majority=args.get('majority'),
+    #             size_kb=old_div(meta.size, 1000))
 
-            res = encode_entity([dataset])
-            return json.loads(res)[0]
-        except Exception as e:
-            return json.loads(e)
+    #         res = encode_entity([dataset])
+    #         return json.loads(res)[0]
+    #     except Exception as e:
+    #         return json.loads(e)
 
 
-    @ns.doc('update a dataset')
-    @api.expect(put_dataset_parser)
-    def put(self):
-        args = put_dataset_parser.parse_args()
-        args['entity_id'] = args.get('id', None)
-        args.pop('id', None)
+    # @ns.doc('update a dataset')
+    # @api.expect(put_dataset_parser)
+    # def put(self):
+    #     args = put_dataset_parser.parse_args()
+    #     args['entity_id'] = args.get('id', None)
+    #     args.pop('id', None)
 
-        # deal with operations
-        args['n_examples_op'] = get_operator_fn(args.get('n_examples_op', None))  # noqa
-        args['k_classes_op'] = get_operator_fn(args.get('n_examples_op', None))
-        args['d_features_op'] = get_operator_fn(args.get('n_examples_op', None))  # noqa
-        args['majority_op'] = get_operator_fn(args.get('n_examples_op', None))
-        args['size_kb_op'] = get_operator_fn(args.get('n_examples_op', None))
+    #     # deal with operations
+    #     args['n_examples_op'] = get_operator_fn(args.get('n_examples_op', None))  # noqa
+    #     args['k_classes_op'] = get_operator_fn(args.get('n_examples_op', None))
+    #     args['d_features_op'] = get_operator_fn(args.get('n_examples_op', None))  # noqa
+    #     args['majority_op'] = get_operator_fn(args.get('n_examples_op', None))
+    #     args['size_kb_op'] = get_operator_fn(args.get('n_examples_op', None))
 
-        dataset = db.update_datasets(**args)
+    #     dataset = db.update_datasets(**args)
 
-        res = encode_entity(dataset)
-        return json.loads(res)
+    #     res = encode_entity(dataset)
+    #     return json.loads(res)
 
 
 if __name__ == '__main__':
