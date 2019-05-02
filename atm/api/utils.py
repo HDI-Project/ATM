72c6ddc226e745e39368d38610c6860ea25faf32
Carles Sala <carles@pythiac.com>
2019-05-02 20:28:33 +0200

    Strip error message

--- atm/api/utils.py
+++ atm/api/utils.py
@@ -13,7 +13,7 @@ def make_absolute(url):
 
 def abort(code, message=None, error=None):
     if error is not None:
-        error = traceback.format_exception_only(type(error), error)[0]
+        error = traceback.format_exception_only(type(error), error)[0].strip()
 
     response = flask.jsonify({
         'status': code,
