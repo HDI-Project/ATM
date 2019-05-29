c9de05f6415625fe5d53b258bebdb032aa56687c
Carles Sala <carles@pythiac.com>
2019-05-29 11:27:15 -0400

    Bump version: 0.1.3-dev → 0.2.0-dev

--- atm/__init__.py
+++ atm/__init__.py
@@ -10,7 +10,7 @@ from atm.core import ATM
 
 __author__ = """MIT Data To AI Lab"""
 __email__ = 'dailabmit@gmail.com'
-__version__ = '0.1.3-dev'
+__version__ = '0.2.0-dev'
 
 # this defines which modules will be imported by "from atm import *"
 __all__ = ['ATM', 'Model', 'config', 'constants', 'data', 'database',
