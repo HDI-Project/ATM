b714a02b769b853f866912e1f4ec5531ea20a087
Carles Sala <carles@pythiac.com>
2019-05-29 10:07:39 -0400

    Make train-path mandatory

--- atm/config.py
+++ atm/config.py
@@ -132,7 +132,10 @@ class DatasetConfig(Config):
     _CONFIG = 'run'
 
     name = 'Given name for this dataset.'
-    train_path = 'Path to raw training data'
+    train_path = {
+        'help': 'Path to raw training data',
+        'required': True
+    }
     test_path = 'Path to raw test data (if applicable)'
     description = 'Description of dataset'
     class_column = ('Name of the class column in the input data', 'class')
