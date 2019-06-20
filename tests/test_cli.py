8251c6a0f34c4f949e36b612e3794b627191f460
Plamen Valentinov <pvkdeveloper@gmx.com>
2019-06-20 18:24:08 +0200

    Fix test for py27

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -14,26 +14,22 @@ def test__get_demos(mock_get_demos):
     mock_get_demos.assert_called_once_with()
 
 
-@patch('atm.cli.print')
 @patch('atm.cli.download_demo')
-def test__download_demo(mock_download_demo, mock_print):
+def test__download_demo(mock_download_demo):
     """Test that the method _download_demo is being called properly with a single dataset."""
 
     # setup
     args_mock = Mock(dataset='test.csv', path=None)
-    mock_download_demo.return_value = 'test.csv'
 
     # run
     cli._download_demo(args_mock)
 
     # assert
     mock_download_demo.assert_called_once_with('test.csv', None)
-    mock_print.assert_called_once_with('Dataset has been saved to test.csv')
 
 
-@patch('atm.cli.print')
 @patch('atm.cli.download_demo')
-def test__download_demo_array(mock_download_demo, mock_print):
+def test__download_demo_array(mock_download_demo):
     """Test that the method _download_demo is being called properly with a two datasets."""
 
     # setup
@@ -44,18 +40,11 @@ def test__download_demo_array(mock_download_demo, mock_print):
     cli._download_demo(args_mock)
 
     # assert
-    expected_print_calls = [
-        call('Dataset has been saved to test.csv'),
-        call('Dataset has been saved to test2.csv')
-    ]
-
     mock_download_demo.assert_called_once_with(['test.csv', 'test2.csv'], None)
-    assert mock_print.call_args_list == expected_print_calls
 
 
-@patch('atm.cli.print')
 @patch('atm.cli.download_demo')
-def test__download_demo_path(mock_download_demo, mock_print):
+def test__download_demo_path(mock_download_demo):
     """Test that the method _download_demo is being called properly with a given path."""
 
     # setup
@@ -66,10 +55,4 @@ def test__download_demo_path(mock_download_demo, mock_print):
     cli._download_demo(args_mock)
 
     # assert
-    expected_print_calls = [
-        call('Dataset has been saved to test.csv'),
-        call('Dataset has been saved to test2.csv')
-    ]
-
     mock_download_demo.assert_called_once_with(['test.csv', 'test2.csv'], 'my_test_path')
-    assert mock_print.call_args_list == expected_print_calls
