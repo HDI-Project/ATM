c98f4920c75810fde0e806d9b0fe9af897d754ac
Carles Sala <carles@pythiac.com>
2019-06-24 09:31:16 -0400

    Bump version: 0.2.1-dev → 0.2.1

--- setup.py
+++ setup.py
@@ -113,6 +113,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/HDI-project/ATM',
-    version='0.2.1-dev',
+    version='0.2.1',
     zip_safe=False,
 )
