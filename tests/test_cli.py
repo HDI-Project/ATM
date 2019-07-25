adf3129ab23311c4144f629ed8e0c5cee09cc2e6
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2019-07-26 00:51:36 +0200

    Test serve / work

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -56,3 +56,39 @@ def test__download_demo_path(mock_download_demo):
 
     # assert
     mock_download_demo.assert_called_once_with(['test.csv', 'test2.csv'], 'my_test_path')
+
+
+@patch('atm.cli._get_atm')
+def test__work(mock__get_atm):
+    # setup
+    args_mock = Mock(dataruns=[1], total_time=[1], save_files=False, cloud_mode=False)
+
+    # run
+    cli._work(args_mock)
+
+    # assert
+    mock__get_atm.assert_called_once_with(args_mock)
+
+    mock__get_atm.return_value.work.assert_called_once_with(
+        datarun_ids=[1],
+        choose_randomly=False,
+        save_files=False,
+        cloud_mode=False,
+        total_time=[1],
+        wait=False
+    )
+
+
+@patch('atm.cli.create_app')
+@patch('atm.cli._get_atm')
+def test__serve(mock__get_atm, mock_create_app):
+    # setup
+    args_mock = Mock(debug=False, host='1.2.3', port='456')
+
+    # run
+    cli._serve(args_mock)
+
+    # assert
+    mock__get_atm.assert_called_once_with(args_mock)
+    mock_create_app.assert_called_once_with(mock__get_atm.return_value, False)
+    mock_create_app.return_value.run.assert_called_once_with(host='1.2.3', port='456')
